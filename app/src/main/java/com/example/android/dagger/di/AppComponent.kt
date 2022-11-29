11d309b95ad6d933d5c2a191781d7c9b8bc0afa0
KashbullinRail <kashbullinrail@gmail.com>
2022-11-29 16:35:23 +0300

    добавлены di интерфейс AppComponent.kt и аннотация @Component

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,12 @@
+package com.example.android.dagger.di
+
+import com.example.android.dagger.registration.RegistrationActivity
+import dagger.Component
+
+
+@Component
+interface AppComponent {
+
+    fun inject(activity: RegistrationActivity)
+
+}
